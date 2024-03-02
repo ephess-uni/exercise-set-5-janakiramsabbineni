5a5ea9d955436c9fb3b8120aaf0230393a1de920
janakiramsabbineni <vsabbine@cbu.edu>
2024-03-02 13:11:56 -0600

    Update ex_5_1.py
    completed

--- src/ex_5_1.py
+++ src/ex_5_1.py
@@ -1,20 +1,24 @@
-"""ex_5_1.py"""
+"""ex_5_2.py
+This module contains an entry point that
+
+- loads data from a file ex_5_2-data.csv into a numpy array
+- shifts and scales the data such that the resulting mean
+        is 0 and the standard deviation is 1.
+- writes the processed data to a file called ex_5_2-processed.csv
+"""
+import numpy as np
+
 try:
-    from src.ex_5_0 import line_count
+    from src.util import get_repository_root
 except ImportError:
-    from ex_5_0 import line_count
+    from util import get_repository_root
 
 
-def main(infile):
-    """Call line_count with the infile argument."""
-    line_count(infile)
-
-
-if __name__ == "__main__":
-    # Create your argument parser object here.
-    # Collect the filename argument from the command line
-    # pass this argument to the main function above
-    # Tests will run your command using a system call.
-    # To test your program with arguments, run it from the command line
-    # (see README.md for more details)
-    pass
+if__name__ == "__main__":
+    root_dir = get_repository_root()
+    INFILE = root_dir / "data" / "ex_5_2-data.csv"
+    OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
+    data = np.loadtxt(INFILE, delimiter=',')
+    data_mean_zero = data - np.mean(data)
+    processed = data_mean_zero / np.std(data_mean_zero)
+    np.savetxt(OUTFILE, processed, delimiter=',')
